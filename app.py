033e30b9c66f1444da6950535675b3cda06f3cbb
Yogesh <iamyogeshgaikwad@gmail.com>
2025-10-22 18:24:33 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Define a route
+@app.route('/')
+def home():
+    return "Hello, Flask! 🚀"
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
